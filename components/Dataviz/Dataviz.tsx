b388299bb3d455967cb438e941c4887a92241c1c
olivierbretaud <olivierbretaud@hotmail.fr>
2021-09-30 17:13:20 +0200

    feat(add Data viz)

--- components/Dataviz/Dataviz.tsx
+++ components/Dataviz/Dataviz.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect } from 'react';
 import * as d3 from 'd3';
+import { Simulation, SimulationNodeDatum } from 'd3-force';
+import styles from './Dataviz.module.scss';
+import useWindowSize from '../../utils/useWindowSize';
 
 type DatavizProps = {
   data: any,
@@ -8,12 +11,13 @@ type DatavizProps = {
 
 const Dataviz = ({ data }: DatavizProps): JSX.Element => {
   console.log(data);
-  const simulation = d3.forceSimulation()
+  const [width, height] = useWindowSize();
+
+  const simulation : Simulation<SimulationNodeDatum, undefined> = d3.forceSimulation()
     .force('link', d3.forceLink()
-      .id((d : any) => d._id)
-      .distance(50)
-      .strength(1))
-    .force('charge', d3.forceManyBody().strength(1));
+      .id((d: any) => d.id))
+    .force('charge', d3.forceManyBody().strength(3))
+    .force('center', d3.forceCenter(width, height));
 
   function drawNodes(initData : any) {
     const dataset = { ...initData };
@@ -21,7 +25,7 @@ const Dataviz = ({ data }: DatavizProps): JSX.Element => {
     d3.select('#container').selectAll(`.${styles.node}`).remove();
 
     d3.select('#graph').selectAll('.link')
-      .data(dataset.links, (link) => link._id)
+      .data(dataset.links, (link: any) => link._id)
       .join(
         (enter) => (
           enter.append('line')
@@ -60,14 +64,14 @@ const Dataviz = ({ data }: DatavizProps): JSX.Element => {
       .attr('class', `${styles.border}`)
       .style('border', (d: any) => `1px solid ${d.color}`);
 
-    const rect = createNodes.append('div')
-      .attr('class', (d: any) => `${styles.rect} ${styles[d.type]}`);
-
     simulation
       .nodes(dataset.nodes);
+    if (!dataset.links.length) {
+      return;
+    }
 
-    simulation.force('link')
-      .links(dataset.links);
+    // simulation.force('link')
+    //   .links(dataset.links);
 
     const numberOfTicks = Math.ceil(Math.log(simulation.alphaMin())
       / Math.log(1 - simulation.alphaDecay()));
@@ -87,34 +91,37 @@ const Dataviz = ({ data }: DatavizProps): JSX.Element => {
     const container = d3.select('#container');
 
     container.selectAll(`.${styles.node}`)
-      .style('transform', (d) => `translate(${width / 2}px,${height / 2}px) scale(0)`)
+      .style('transform', `translate(${width / 2}px,${height / 2}px) scale(0)`)
       .style('opacity', 0)
       .transition()
       .delay((d, i) => i * 50)
       .duration(300)
       .style('opacity', 1)
-      .style('transform', (d) => `translate(${d.x}px,${d.y}px) scale(1)`);
+      .style('transform', (d : any) => `translate(${d.x}px,${d.y}px) scale(1)`);
 
     const graph = d3.select('#graph');
 
     graph.selectAll('.link')
       .style('opacity', 0)
-      .attr('x1', (d) => width / 2)
-      .attr('y1', (d) => height / 2)
-      .attr('x2', (d) => width / 2)
-      .attr('y2', (d) => height / 2)
+      .attr('x1', width / 2)
+      .attr('y1', height / 2)
+      .attr('x2', width / 2)
+      .attr('y2', height / 2)
       .transition()
       .delay((d, i) => 250 + i * 50)
       .duration(300)
       .style('opacity', 1)
-      .attr('x1', (d) => d.source.x)
-      .attr('y1', (d) => d.source.y)
-      .attr('x2', (d) => d.target.x)
-      .attr('y2', (d) => d.target.y);
+      .attr('x1', (d: any) => d.source.x)
+      .attr('y1', (d: any) => d.source.y)
+      .attr('x2', (d: any) => d.target.x)
+      .attr('y2', (d: any) => d.target.y);
   }
 
   return (
-    <div id="container">
+    <div id='container' className={styles.container}>
+      <svg width={width} height={height}>
+        <g id='graph' ></g>
+      </svg>
     </div>
   );
 };
