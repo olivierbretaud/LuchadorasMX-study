6de141797feda90d0fc38f5b406d83b9d8ddb0bc
olivierbretaud <olivierbretaud@hotmail.fr>
2021-10-06 14:07:58 +0200

    fix(Panel) display post

--- components/Panel/Panel.tsx
+++ components/Panel/Panel.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect } from 'react';
+import React from 'react';
+import { FacebookProvider, EmbeddedPost } from 'react-facebook';
 import styles from './Panel.module.scss';
 
 export default function Panel({ post }: {
@@ -9,12 +10,11 @@ export default function Panel({ post }: {
     <div className={`${styles.panel} ${post ? styles['is-open'] : ''}`}>
       {post?.id
        && <div className={styles.post}>
-        <iframe
-        id={'iframe'}
-        src={`https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F140301286056129%2Fposts%2F${post.id}%2F&width=380&show_text=true&height=200&appId`}
-        width="380" style={{ border: 'none', overflow: 'hidden' }} frameBorder="0" scrolling="yes" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-      </div>
+          <FacebookProvider appId="406497424294475">
+            <EmbeddedPost href={`https://www.facebook.com/140301286056129/posts/${post?.id}`} width="380" />
+          </FacebookProvider>
+        </div>
       }
-      </div>
+    </div>
   );
 }
