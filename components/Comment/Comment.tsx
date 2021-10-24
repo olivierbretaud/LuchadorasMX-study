ad6ac34f419ba881974ca4ef54ed22ec6c732a40
olivierbretaud <olivierbretaud@hotmail.fr>
2021-10-24 14:24:13 +0200

    feat(Comment)

---
+++ components/Comment/Comment.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState, useRef } from 'react';
+import { IoChevronDownSharp } from 'react-icons/io5';
+import styles from './Comment.module.scss';
+
+const Comment = ({ comment } : { comment: any }): JSX.Element => {
+  console.log(comment);
+  const [detailIsOpen, setDetailIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+  return (
+  <div className={styles.container}>
+    <p>{comment.comment}</p>
+    <div className={styles.author}>
+          {comment?.ideologies.length > 0
+            ? <div className={styles.ideologies}>
+              <div className={styles.list}>
+              {comment.author.ideologies.map((y : string, i :number) => <p
+                key={comment.author.name + y + i}>
+                {y}
+              </p>)}
+            </div>
+          </div>
+            : <div>
+          </div>
+          }
+      <button
+        onClick={() => setDetailIsOpen(!detailIsOpen)}
+        >
+        <IoChevronDownSharp size={20}/>
+      </button>
+    </div>
+    <div className={styles.panel}
+      style={{
+        height: detailIsOpen && contentRef.current ? contentRef.current?.clientHeight : 0,
+      }}>
+        <div className={styles.content} ref={contentRef}>
+          <p className={styles.name}>
+            {comment.author.name}
+          </p>
+          <p>
+          {comment.author?.gender && <span> Genre : {comment.author.gender}<br/></span>}
+          {comment.author?.age && <span> Age : {comment.author.age}<br/></span>}
+          {comment.author?.country && <span>Pays : {comment.author.country}<br/></span>}
+          {comment.author?.region && <span>Region : {comment.author.region}<br/></span>}
+          </p>
+
+          {comment.author?.ideologies.length > 0
+            && <div className={styles.ideologies}>
+              <p>Idéologies</p>
+              <div className={styles.list}>
+              {comment.author.ideologies.map((y : string, i :number) => <p
+                key={comment.author.name + y + i}>
+                {y}
+              </p>)}
+            </div>
+          </div>
+          }
+        </div>
+    </div>
+  </div>
+  );
+};
+
+export default Comment;
--- components/Panel/Panel.tsx
+++ components/Panel/Panel.tsx
@@ -1,25 +1,38 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
+import { IoClose } from 'react-icons/io5';
 import Link from 'next/link';
 import { FacebookProvider, EmbeddedPost, Comments } from 'react-facebook';
 import styles from './Panel.module.scss';
+import Comment from '../Comment/Comment';
 
-export default function Panel({ post }: {
+export default function Panel({ post, data }: {
   post: any;
   data: any;
 }): JSX.Element {
+  console.log(data);
   return (
     <div className={`${styles.panel} ${post ? styles['is-open'] : ''}`}>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
+
+      <div className={styles.close} >
+        <Link href="/">
+          <a >
+            FERMER <IoClose size={22}/>
+          </a>
+        </Link>
+      </div>
       {post?.id
-       && <div className={styles.post}>
+       && <>
+          <div className={styles.post}>
           <FacebookProvider appId="406497424294475">
             <EmbeddedPost href={`https://www.facebook.com/140301286056129/posts/${post?.id}`} width="380" />
             <Comments href={'https://www.facebook.com/LuchadorasMX/videos/617679528920076/?comment_id=2719824454941168'} width="380" />
           </FacebookProvider>
         </div>
+        <div className={styles['comment-list']}>
+          {data.nodes?.filter((d: any) => d.type === 'comment').map((c : any) => <Comment key={c.id} comment={c} />)}
+        </div>
+        </>
       }
     </div>
   );
