b6fb691c5e0604b65c6c2b2de201dca1bdbec03b
olivierbretaud <olivierbretaud@hotmail.fr>
2022-01-31 11:45:35 +0100

    revome stats

--- components/Stats/Stats.tsx
+++ components/Stats/Stats.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState, useEffect } from 'react';
+import * as d3 from 'd3';
 import { ResponsivePie } from '@nivo/pie';
 import { IoClose } from 'react-icons/io5';
 import styles from './Stats.module.scss';
@@ -12,6 +13,13 @@ const Stats = ({ data } : { data: any}): JSX.Element => {
   const [statsIsOpen, setStatsIsOpen] = useState(false);
   const [stats, setStats] = useState<Stats>();
 
+  // function colorScale(value: number, max: number) {
+  //   const color = d3.scaleOrdinal() // D3 Version 4
+  //     .domain([0, max])
+  //     .range(['#FF0000', '#009933']);
+  //   return color(value);
+  // }
+
   useEffect(() => {
     if (data?.list && data?.nodes) {
       const authors = data.nodes.filter((n: any) => n.type === 'author');
@@ -22,6 +30,10 @@ const Stats = ({ data } : { data: any}): JSX.Element => {
             id: g,
             label: g,
             value: authors.filter((n: any) => n.gender === g)?.length,
+            // color: colorScale(
+            //   authors.filter((n: any) => n.gender === g).length,
+            //   100,
+            // ),
           });
         }
       });
@@ -58,6 +70,7 @@ const Stats = ({ data } : { data: any}): JSX.Element => {
               }}
               innerRadius={0.8}
               colors={{ scheme: 'purples' }}
+              // colors={{ datum: 'data.color' }}
               padAngle={0.7}
               sortByValue={true}
               cornerRadius={0}
